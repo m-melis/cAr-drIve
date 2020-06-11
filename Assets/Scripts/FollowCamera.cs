d42697fc2e39bce8ba7ea680bab829e4260cff13
Marco Melis <marco.melis@unica.it>
2020-06-11 16:44:08 +0200

    Added new script to implement a followUp camera.

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Vector3 myPos;
+    public Transform myPlay;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = myPlay.position + myPos;
+    }
+}
